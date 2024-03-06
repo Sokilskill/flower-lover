c3e87c7ad551489c74e237ba82548f8d5f9a0f45
Sokilskill <vprovorotov@gmail.com>
2024-03-06 20:27:45 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <img
+        src="/src/assets/images/logo.svg"
+        alt="logo"
+        width="200"
+        height="200"
+      />
+      ;
+    </>
+  );
+}
+
+export default App;
