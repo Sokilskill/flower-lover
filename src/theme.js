ef8213747f5e7ce7636e7a81e281196c03147fc1
Sokilskill <vprovorotov@gmail.com>
2024-03-08 20:31:49 +0200

    fix breakpoint

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme({
+export const mytheme = extendTheme({
   breakpoints: {
-    base: "0px",
-    sm: "390px", //  mobile
-    md: "768px", //  tablet
-    lg: "1024px", //  laptop
-    xl: "1440px", //  desktop
+    base: "0em",
+    sm: "24em", // 390px / 16px = 24.375em
+    md: "48em", // 768px / 16px = 48em
+    lg: "64em", // 1024px / 16px = 64em
+    xl: "90em", // 1440px / 16px = 90em
   },
   fonts: {
     body: `"Source Sans", sans-serif`,
@@ -27,5 +27,3 @@ export const theme = extendTheme({
     },
   },
 });
-
-export default theme;
