8930af90869d80521d121dbed8cc151b937d8f79
Sokilskill <vprovorotov@gmail.com>
2024-03-09 12:14:15 +0200

    create component MobMenu

---
+++ src/components/Header/MobMenu/MobMenu.jsx
@@ -0,0 +1,22 @@
+import { Flex, IconButton } from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
+import BurgerMenu from "../../../BurgerButton/BurgerMenu";
+
+export function MobMenu() {
+  return (
+    <Flex alignItems="center" gap={4}>
+      <IconButton
+        as="a"
+        href="#catalog"
+        icon={<FiSearch />}
+        aria-label="Search"
+        w="42px"
+        h="42px"
+        colorScheme=""
+        bg={"inherit"}
+        fontSize="20px"
+      />
+      <BurgerMenu />
+    </Flex>
+  );
+}
