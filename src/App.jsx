24530f39c321a6a31300b16d87eeb7edc2eed110
Sokilskill <vprovorotov@gmail.com>
2024-03-24 02:01:59 +0200

    added other components
    додані компоненти

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Header from "./components/Header/Header";
 import { SectionHero } from "./components/SectionHero/SectionHero";
 
+import { SectionCatalog } from "./components/SectionCatalog/SectionCatalog";
+import { SectionClients } from "./components/SectionClients/SectionClients";
+import { SectionExploringQuality } from "./components/SectionExploringQuality/SectionExploringQuality";
+import { SectionHelp } from "./components/SectionHelp/SectionHelp";
+import { Footer } from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <Header />
       <SectionHero />
+      <SectionCatalog />
+      <SectionExploringQuality />
+      <SectionHelp />
+      <SectionClients />
+      <Footer />
     </>
   );
 }
